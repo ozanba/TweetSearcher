e29ef20bd2e1e0fe82fa3e1bfaf62c85c020a8fb
ozanba <ozanbagiran@gmail.com>
2024-01-29 16:57:59 +0300

    new features
    new features added.
    Lang Code and info image  button added.

--- src/searchGUI.java
+++ src/searchGUI.java
@@ -1,28 +1,39 @@
 
 //  Created by Ozan Bağıran
 import java.awt.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 public class searchGUI extends JFrame {	
 	
 	
-	private JLabel accountL, sDateL, eDateL;
-	private JTextField accountTf, sDateTf, eDateTf;
+	private JLabel accountL, sDateL, eDateL, wordL, langL;
+	private JTextField accountTf, sDateTf, eDateTf, wordTf, langTf;
 	private JButton showB;
 	
-	public searchGUI() {
+	public searchGUI()  {
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout());
 		
-		setTitle("asas ");
-        setSize(250, 220);
+		setTitle("Tweet Searcher");
+        setSize(250, 320);
+        setResizable(false);
         setVisible(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
 		accountL = new JLabel("Account: ");
 		accountTf = new JTextField(20);
+		
+		wordL = new JLabel("Word: ");
+		wordTf = new JTextField(20);
 		
 		sDateL = new JLabel("Start Date:");
 		sDateTf = new JTextField("YYYY-MM-DD",20);
@@ -30,10 +41,20 @@ public class searchGUI extends JFrame {
 		eDateL = new JLabel("End Date:");
 		eDateTf = new JTextField("YYYY-MM-DD",20);
 		
+		langL = new JLabel("Lang Code: ");
+		langTf = new JTextField(16);
+		
+		
+		
+ 
 		showB = new JButton("Show");
 		
 		c.add(accountL);
 		c.add(accountTf);
+		
+		c.add(wordL);
+		c.add(wordTf);
+		
 		
 		c.add(sDateL);
 		c.add(sDateTf);
@@ -41,10 +62,42 @@ public class searchGUI extends JFrame {
 		c.add(eDateL);
 		c.add(eDateTf);
 		
+		c.add(langL);
+		c.add(langTf);
+		
+		
+		 String photoPath = "info.png";
+	        try {
+	            BufferedImage img = ImageIO.read(new File(photoPath));
+	            ImageIcon icon = new ImageIcon(img.getScaledInstance(20, 20, Image.SCALE_SMOOTH));
+
+	            JLabel label = new JLabel(icon);
+
+	            label.addMouseListener(new MouseAdapter() {
+	                @Override
+	                public void mouseClicked(MouseEvent e) {
+	                    JOptionPane.showMessageDialog(label, ""
+	                    		+ "English: en\n"
+	                    		+ "Turkish: tr\n"
+	                    		+ "German: de\n...\n"
+	                    		+ "Not Required to fill");
+	                }
+	            });
+	            c.add(label);
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		
+		
+		
 		c.add(showB);
 		
 		MyEventHandler meh = new MyEventHandler();
 		showB.addActionListener(meh);
+		
+		
+		
 		
 		
 	}
@@ -52,13 +105,15 @@ public class searchGUI extends JFrame {
 	public class MyEventHandler implements ActionListener{
 		public void actionPerformed(ActionEvent e){
 			String component = "";
-			if (e.getSource() == showB) {
+			if (e.getSource() == showB){
 				if (!isValidDateFormat(sDateTf.getText()) || !isValidDateFormat(eDateTf.getText())) {
 	                JOptionPane.showMessageDialog(searchGUI.this, "Invalid date format. Please use YYYY-MM-DD");
 	                return; // Exit the method if date format is invalid
 	            }
-				component= "(from%3A"+accountTf.getText()+")%20until%3A"+eDateTf.getText()+"%20since%3A"+sDateTf.getText();
-				
+				if(!langTf.getText().isEmpty())
+					component= "(from%3A"+accountTf.getText()+")%20until%3A"+eDateTf.getText()+"%20since%3A"+sDateTf.getText()+"%20"+wordTf.getText()+"%20lang%3A"+langTf.getText();
+				else
+					component= "(from%3A"+accountTf.getText()+")%20until%3A"+eDateTf.getText()+"%20since%3A"+sDateTf.getText()+"%20"+wordTf.getText();
 				component = "https://twitter.com/search?q="+ component+"&src=typed_query&f=top";
 			OpenURLInBrowser.openURL(component);	
 				
